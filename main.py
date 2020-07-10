d4fab06ba62d476323e173117eb64917e66bcbb3
StepanovSA <stepka45@gmail.com>
2020-07-10 18:07:03 +0500

    init

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ import os
 # file_name = Path(r'\\nts2dc\W1User\Ficep_2\11824_1-01.fnc')
 # file_name = Path(r'\\nts2dc\W1User\Ficep_2\11815_1-01.fnc')
 # file_name = Path(r'\\nts2dc\W1User\Ficep_2\10181_1-01.fnc')
-TASKS_PATH = Path(r'd:\Workdir')
+# TASKS_PATH = Path(r'd:\Workdir')
+TASKS_PATH = Path(r'k:\Ficep_2')
 DELTA = 1000
+LINES_LIST = []
+RK_INFO = {}
 
 def get_tasks():
     root = Tk()
@@ -21,29 +24,61 @@ def get_tasks():
                                                filetypes=(("ods files", "*.fnc"), ("all files", "*.*")))
     return root.filenames
 
+
+def str_to_int_float(str):
+    if '.' in str:
+        return float(str)
+    else:
+        return int(str)
+
+
 def get_dct_holes(path):
+    global LINES_LIST
+    global RAZM_LIST
     holes_dict = {}
+    xl_list = []
+    yl_list = []
     with open(path) as f:
         lines = f.readlines()  # list containing lines of file
         for line in lines:
+            line = line.strip()
+            if 'C:' in line:
+                RK_INFO['Name'] = line
+            if 'LP' in line and 'SA' in line:
+                line = line.split()
+                RK_INFO['S']=str_to_int_float(line[2][2:])
+                RK_INFO['X']=str_to_int_float(line[1][2:])
+                RK_INFO['Y']=str_to_int_float(line[0][2:])
+            if 'SKEL' in line:
+                line = line.strip('[SKEL] TS51 X')
+                if RK_INFO.get('Skel'):
+                    RK_INFO['Skel'].append(str_to_int_float(line))
+                else:
+                    RK_INFO['Skel'] = []
+                    RK_INFO['Skel'].append(str_to_int_float(line))
             if '[HOL]' in line:
                 line = line.strip('[HOL] ')
-            line = line.strip()
+            if '[CUT]' in line:
+                line = line.strip('[CUT] ')
+
+            if 'R0' in line and '-1401' not in line:
+                line = line.split()
+                xl_coord = str_to_int_float(line[0][1:])
+                yl_coord = str_to_int_float(line[1][1:])
+                xl_list.append(xl_coord)
+                yl_list.append(yl_coord)
+            if 'UNLO' in line:
+                LINES_LIST.append(xl_list)
+                LINES_LIST.append(yl_list)
+                xl_list = []
+                yl_list = []
+                # LINES_LIST.append(xl_list)
             if 'TS33' in line:
                 line = line.split()[1:]
                 # print(line)
                 dia = line[0][2:]
-                x_coord = line[1][1:]
-                y_coord = line[2][1:]
-
-                if '.' in x_coord:
-                    x_coord = float(x_coord)
-                else:
-                    x_coord = int(x_coord)
-                if '.' in y_coord:
-                    y_coord = float(y_coord)
-                else:
-                    y_coord = int(y_coord)
+                x_coord = str_to_int_float(line[1][1:])
+                y_coord = str_to_int_float(line[2][1:])
 
                 if not holes_dict.get(dia):
                     holes_dict[dia] = {}
@@ -138,7 +173,7 @@ def save(name='', fmt='png'):
 if __name__ == '__main__':
     file_name = get_tasks()
     dct_input = get_dct_holes(file_name)
-    print(dct_input)
+    # print(dct_input)
 
     summa_par = split_dicts(dct_input)
     print(summa_par)
@@ -146,9 +181,21 @@ if __name__ == '__main__':
     dict_num_holes = count_holes(dct_input)
     print(dict_num_holes)
 
+    # figsize = (11.69, 8.27)
+    fig = plt.figure(figsize=(56 , 25), dpi=50)
+    fig.set_size_inches(11.69, 8.27)
+    fig.set_facecolor('lightgrey')
 
-    fig = plt.figure(figsize=(60, 40), dpi=100)
 
+    ax = fig.add_subplot(111)
+    ax.set_facecolor('lightgrey')
+    ax.set_xlim([0, RK_INFO['Y']])
+    ax.set_ylim([0, RK_INFO['X']])
+    ax.set_title(RK_INFO['Name'] + '  S:' + str(RK_INFO['S']))
+    ax.title.set_size(30)
+    ax.set_xticks(RK_INFO['Skel'])
+    # fig = plt.figure((figsize=)
+    # print(plt.figaspect(2.0))
     # scatter1 = plt.scatter(0.0, 1.0)
     # graph1 = plt.plot([-1.0, 1.0], [0.0, 1.0])
     # text1 = plt.text(0.5, 0.5, 'Text on figure')
@@ -166,14 +213,15 @@ if __name__ == '__main__':
     for dia, dct_inner in dct_input.items():
         for x_coord, y_coord_list in dct_inner.items():
             for y_coord in y_coord_list:
-                size = int(dia)*1.5
+                size = int(dia)
                 x_coordinates.append(x_coord)
                 y_coordinates.append(y_coord)
                 size_map.append(size)
 
-    scatter1 = plt.scatter(x_coordinates, y_coordinates, s=size_map, edgecolors='none', c='b')
-    grid1 = plt.grid(True)   # линии вспомогательной сетки
-
+    ax.scatter(x_coordinates, y_coordinates, s=size_map, edgecolors='none', c='b')
+    # grid1 = plt.grid(True)   # линии вспомогательной сетки
+    # print(*LINES_LIST)
+    ax.plot(*LINES_LIST, c='black')
     # save(name='pic_2_1', fmt='pdf')
     # save(name='pic_2_1', fmt='png')
 
